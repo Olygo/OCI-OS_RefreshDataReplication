632853262569e37299ab26acbb5039c87582e488
Olygo <florian@olygo.com>
2023-09-20 03:08:56 +0200

    1.0.0
    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# OCI-OS_RefreshDataReplication
- Force the replication of existing data from one OCI region to another.
+# OCI Object Storage Refresh Data Replication
+
+This Bash script is designed to facilitate the replication of data from one OCI region to another. The typical use case is when you enable an OCI replication policy, but data already exist in the bucket, in this case only newly created data is being replicated to the second region. To address this situation and force OCI to replicate data that existed before enabling the replication policy, this script downloads the existing files locally and then uploads them back, triggering the replication process.
+
+This README.md file provides an explanation of the script's functionality, instructions on how to use it, and details regarding its limitations and constraints.
+
+This script runs :
+
+- within Oracle Cloud Infrastructure (OCI) CloudShell
+	- /!\ Disk space is limited to 4GB maximum
+- on compute instance with instance_principal authentication
+- on compute instance with config_file authentication
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Usage](#usage)
+- [Limitations](#limitations)
+- [License](#license)
+- [Disclaimer](#disclaimer)
+- [Questions](#questions)
+
+---
+
+## Introduction
+
+The OCI OS Refresh Data Replication Script performs the following key actions:
+
+1. Retrieves the OCI Object Storage namespace.
+2. Retrieves the approximate size of the specified bucket.
+3. Checks the available disk space on the CloudShell file system.
+4. Downloads files from the specified bucket to a temporary directory on the CloudShell file system.
+5. Controls there is no error during the download process.
+6. Uploads files from the temporary directory back to the specified bucket.
+7. Controls there is no error during the upload process
+8. Displays the total data transferred.
+9. Cleans up the temporary directory on the local file system.
+
+## Usage
+
+To use the script, follow these steps:
+
+1. Open an OCI CloudShell session or ssh into a compute instance
+2. Download script locally
+
+   ```bash
+   curl https://raw.githubusercontent.com/Olygo/OCI-OS_RefreshDataReplication/main/oci_os_rdr.sh -o ./oci_os_rdr.sh && chmod +x ./oci_os_rdr.sh
+   ```
+3. Run the script from OCI CloudShell with the following command:
+
+   ```bash
+   ./oci_os_rdr.sh <bucket_name>
+   ```
+4. Run the script within a compute instance using config_file authentication:
+
+   ```bash
+   ./oci_os_rdr.sh <bucket_name> cf
+   ```
+5. Run the script within a compute instance using instance_principal authentication:
+
+   ```bash
+   ./oci_os_rdr.sh <bucket_name> ip
+   ```
+
+   Replace `<bucket_name>` with the name of the OCI Object Storage bucket you want to transfer data to and from.
+
+6. The script will perform the following tasks:
+   - Retrieve the OCI Object Storage namespace.
+   - Check if the specified bucket exists.
+   - Calculate the approximate size of the bucket.
+   - Check the available disk space on the local file system (~).
+   - Download files from the bucket to a temporary directory (~/<timestamp>_<bucket_name>).
+   - Upload files from the temporary directory back to the bucket.
+   - Display the total data transferred.
+   - Clean up the temporary directory.
+
+## License
+
+This script is provided under the [MIT License](LICENSE). You are free to use, modify, and distribute it as per the terms of the license.
+
+
+## Disclaimer
+**Please test properly on test resources, before using it on production resources to prevent unwanted outages or unwanted bills.**
+
+
+## Questions ?
+Please feel free to reach out for any clarifications or assistance in using this script in your OCI CloudShell environment.
+
+**_olygo.git@gmail.com_**
---
+++ oci_os_rdr.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+config_file="~/.oci/config"
+
+# Expand path
+eval config_file="$config_file"
+
+# Check arguments
+if [ $# -lt 1 ]; then
+    echo
+    echo "Missing argument(s)..."
+    echo "Usage: $0 bucket_name auth_method"
+    echo
+    echo "      $0 bucket_name ip | cf"
+    echo
+    exit 1
+fi
+
+# Assign the first argument to bucket_name
+bucket_name="$1"
+
+# Check authentication argument
+if [ -n "$2" ]; then
+    if [ "$2" = "ip" ]; then
+        auth="--auth instance_principal"
+        echo
+        echo "auth_method = Instance principal"
+        echo
+    elif [ "$2" = "cf" ]; then
+        if [[ -f $config_file ]]; then
+            auth="--auth api_key"
+            echo
+            echo "auth_method = Config file"
+            echo
+        else
+            echo " /!\ CONFIG FILE NOT FOUND: $config_file"
+            exit 1
+        fi
+    else
+        echo
+        echo " /!\ INVALID AUTH ARGUMENT: $2"
+        echo
+        exit 1
+    fi
+else
+    auth=""
+fi
+
+echo
+echo "= = = = = = = RETRIEVING NAMESPACE = = = = = = ="
+echo
+
+oci_output=$(oci os ns get $auth)
+
+namespace=$(echo "$oci_output" | jq -r '.data')
+
+if [ -z "$namespace" ]; then
+   echo
+   echo "/!\ UNABLE TO GET NAMESPACE, CHECK AUTHENTICATION METHOD"
+   echo
+   exit 1
+else
+   echo " $namespace"
+fi
+
+echo
+echo "= = = = = = = RETRIEVING BUCKET SIZE = = = = = = ="
+echo
+
+get_bucket_size=$(oci os bucket get -bn $bucket_name -ns $namespace --fields 'approximateSize' $auth)
+
+# Check if ServiceError while getting bucket size => usually because bucket does't exist
+if echo "$get_bucket_size" | jq -e 'contains("ServiceError")' > /dev/null 2>&1; then
+    # oci will display the detailed error here
+    echo
+    echo "Or maybe the bucket is located in another region ?"
+    echo
+    exit 1
+else
+    # Retrieve approximate-size from json (Bytes)
+    bucket_size=$(echo "$get_bucket_size" | jq -r '.data."approximate-size"')
+
+    # Convert integer to float with 2 decimal places & bucket_size from Bytes to Gigabytes
+    bucket_size_gb=$(awk "BEGIN {printf \"%.2f\", $bucket_size/1024/1024/1024}")
+    bucket_size_kb=$(awk "BEGIN {printf \"%.2f\", $bucket_size/1024}")
+    
+    echo " Bucket "$bucket_name" approximate size is: $bucket_size_gb"GB
+    
+    echo
+    echo "= = = = = = = CHECK LOCAL DISK FREE SPACE in ~ (/home/username) = = = = = = ="
+    echo
+    available_space_kb=$(df -BK ~ | awk 'NR==2 {print $4}' | sed 's/K//')
+    available_space_gb=$(df -BG ~ | awk 'NR==2 {print $4}' | sed 's/G//')
+
+    # Check if local_disk_space >= $bucket_size_gb
+
+    # Convert available_space_kb and bucket_size_kb to integers
+    available_space_int=${available_space_kb%.*} # Remove decimal point
+    bucket_size_int=${bucket_size_kb%.*} # Remove decimal point
+
+    if [ "$available_space_int" -ge "$bucket_size_int" ]; then
+        echo "Local disk space ok -  Local: $available_space_int / bucket_size: $bucket_size_int"
+    else
+        echo
+        echo " /!\ NOT ENOUGH DISK SPACE -  Local: $available_space_kb / bucket_size: $bucket_size_kb"
+        echo
+        exit 1
+    fi
+
+    tmp_dir=$(date +%d%m%y%H%M%S)_$bucket_name
+    mkdir ~/$tmp_dir
+
+    echo
+    echo "= = = = = = = DOWNLOADING FILES IN PROGRESS = = = = = = ="
+    echo
+    download_output=$(oci os object bulk-download -bn $bucket_name --dest-dir ~/$tmp_dir -ns $namespace $auth)
+
+    # Check if download_failures is empty or not
+    if [ "$(echo "$download_output" | jq '.["download-failures"] | length > 0')" = true ]; then
+
+        echo
+        echo "$download_output" | jq -r '.["download-failures"]'
+        echo
+        echo " /!\ download_failures found please check :"
+        echo "                                        - error above"
+        echo "                                        - local disk space"
+        echo "                                        - real bucket size"
+        echo
+        echo " /!\ delete temporary data in: ~/$tmp_dir"
+        echo
+        exit 1
+    else
+        echo
+        #echo $download_output
+        echo
+        echo "No download_failures found, continuing"
+        echo
+    fi
+
+    echo
+    echo "= = = = = = = UPLOADING FILES IN PROGRESS = = = = = = ="
+    upload_output=$(oci os object bulk-upload -bn $bucket_name --src-dir ~/$tmp_dir -ns $namespace --overwrite --verify-checksum $auth)
+
+    # Check if upload_failures is empty or not
+    if [ "$(echo "$upload_output" | jq '.["upload-failures"] | length > 0')" = true ]; then
+
+        echo
+        echo "$upload_output" | jq -r '.["upload-failures"]'
+        echo
+        echo " /!\ upload_failures found please check :"
+        echo "                                        - error above"
+        echo "                                        - local disk space"
+        echo "                                        - real bucket size"
+        echo
+    else
+        echo
+        #echo $upload_output
+        echo
+        echo "No upload_failures found, continuing"
+    fi
+
+    echo
+    echo "= = = = = = = TOTAL DATA TRANSFERED = = = = = = ="
+    echo
+    du -h -d1 ~/$tmp_dir
+
+    echo
+    echo "= = = = = = = CLEANING LOCAL FILES = = = = = = ="
+    echo
+    rm -rdf ~/$tmp_dir
+    echo " Local files deleted..."
+    echo
+fi
